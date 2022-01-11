cae4d1e5964debef4ae050ba51086c79cd925cd7
Wesley <leewes5928@users.noreply.github.com>
2022-01-11 15:50:52 +0900

    Refactor to use method getResource for diet-api service.

--- src/app/Interface.ts
+++ src/app/Interface.ts
@@ -1,28 +1,28 @@
 export interface Meal {
-    id?: number;
-    users_id: number;
-    week: number;
-    days_id: number;
-    carbs_id: number;
-    created_at?: string;
-    updated_at?: string;
+  id?: number;
+  users_id: number;
+  week: number;
+  days_id: number;
+  carbs_id: number;
+  created_at?: Date;
+  updated_at?: Date;
 }
 
 export interface User {
-    id?: number;
-    first_name: string;
-    last_name: string;
-    dob?: Date;
+  id?: number;
+  first_name: string;
+  last_name: string;
+  dob?: Date;
 }
 
 export interface Day {
-    id?: number;
-    name: string;
+  id?: number;
+  name: string;
 }
 
 export interface Carb {
-    id?: number;
-    breakfast: number;
-    lunch: number;
-    dinner: number;
+  id?: number;
+  breakfast: number;
+  lunch: number;
+  dinner: number;
 }
--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { Meal } from '../Interface';
+import { Meal, User } from '../Interface';
 import { DietApiService } from '../services/diet-api.service';
 
 @Component({
   selector: 'app-graph',
   templateUrl: './graph.component.html',
-  styleUrls: ['./graph.component.css']
+  styleUrls: ['./graph.component.css'],
 })
 export class GraphComponent implements OnInit {
-  meals: Meal[] = [];
+  meals: Meal[] = [{ users_id: -1, week: -1, days_id: -1, carbs_id: -1 }];
   constructor(private dietApiService: DietApiService) {}
 
   ngOnInit(): void {
-    this.dietApiService.getMeals().subscribe((meal) => this.meals = meal)
+    this.dietApiService
+      .getResource('users')
+      .subscribe((meals) => (this.meals = meals));
   }
 }
