cc6660898d3def934dfd34f5a54fee8f432328bc
Wesley <leewes5928@users.noreply.github.com>
2022-01-12 12:25:49 +0900

    Add user-menu component.

---
+++ src/app/user-menu/user-menu.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from '../Interface';
+
+@Component({
+  selector: 'app-user-menu',
+  templateUrl: './user-menu.component.html',
+  styleUrls: ['./user-menu.component.css']
+})
+export class UserMenuComponent implements OnInit {
+  @Input() users: User[] = [];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
